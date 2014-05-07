d0a36485f415ef8b443405f5bd1a6777a089f6f3
YonaChii <iona.f.concepcion@gmail.com>
2014-05-07 10:15:12 +0800

    Log-in, log-out works. User authentication worksgit status :)

--- src/exam/app/urls.py
+++ src/exam/app/urls.py
@@ -3,9 +3,9 @@ from app import views
 
 urlpatterns = patterns('',
     url(r'^$', views.index, name='index'),
-    url(r'^login/$', views.login, name='login'),
+    url(r'^login/$', views.login_view, name='login'),
     url(r'^(?P<exam_id>\d+)/$', views.view, name='view'),
     url(r'^(?P<exam_id>\d+)/result/$', views.result, name='result'),
     url(r'^(?P<exam_id>\d+)/calc/$', views.calc, name='calc'),
-    url(r'^logout/$', views.login, name='logout'),
+    url(r'^logout/$', views.logout_view, name='logout'),
 )
--- src/exam/app/views.py
+++ src/exam/app/views.py
@@ -1,40 +1,57 @@
 from django.contrib.auth import authenticate, login, logout
-from django.shortcuts import get_object_or_404, render
+from django.shortcuts import get_object_or_404, render, redirect
 from django.http import HttpResponseRedirect
 from django.core.urlresolvers import reverse
 from django.views import generic
 from app.models import Exam
 
 def index(request):
-    exam_list = Exam.objects.all()
-    return render(request, 'app/index.html', {'exam_list': exam_list})
+    if request.user.is_authenticated():
+        exam_list = Exam.objects.all()
+        return render(request, 'app/index.html', {'exam_list': exam_list})
+    else:
+        return render(request, 'app/login.html', {'not_in':True})
+        #return redirect('app:login')        
 
 def view(request, exam_id):
-    exam = get_object_or_404(Exam, pk=exam_id)
-    return render(request, 'app/view.html', {'exam': exam})
+    if request.user.is_authenticated():
+        exam = get_object_or_404(Exam, pk=exam_id)
+        return render(request, 'app/view.html', {'exam': exam})
+    else:
+        return render(request, 'app/login.html', {'not_in':True})
 
 def result(request, exam_id):
-    exam = get_object_or_404(Exam, pk=exam_id)
-    return render(request, 'app/result.html', {'exam': exam})
-
-def login(request):
-    username = request.POST['username']
-    password = request.POST['password']
-    user = authenticate(username=username, password=password)
-    if user is not None:
-	if user.is_active:
-	    login(request, user)
-	    return redirect('app:index')
-	else:
-	    return HttpResponse('Disabled Account')
+    if request.user.is_authenticated():
+        exam = get_object_or_404(Exam, pk=exam_id)
+        return render(request, 'app/result.html', {'exam': exam})
     else:
-	return render(request, 'app:login', {'invalid':True})
+        return render(request, 'app/login.html', {'not_in':True})
 
-def logout(request):
+def login_view(request):
+    if request.user.is_authenticated():
+        return redirect('app:index')
+    if request.method == 'GET':
+        return render(request, 'app/login.html')
+    elif request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(username=username, password=password)
+        if user is not None:
+	    if user.is_active:
+	        login(request, user)
+                return HttpResponseRedirect(reverse('app:index'))
+	        #return redirect('app:index')
+	    else:
+	        return HttpResponse('Disabled Account')
+        else:
+	    return render(request, 'app/login.html', {'invalid':True})
+
+def logout_view(request):
     logout(request)
+    return redirect('app:login')
 
 def calc(request, exam_id):
-    #calculate score
-    #store score get average
+    #take in varaibles answers and number of questions
+    #calculate total score
     #inc exam.times_taken
-    return HttpResponse("calculating exam score")
+    return redirect('app:index')
